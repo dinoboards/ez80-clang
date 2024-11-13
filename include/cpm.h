64d93f45f4425cd07ed10ca92c12b84c361f9911
Dean Netherton <dinoboards@netherton.org>
2024-11-13 18:41:55 +1100

    libcpm: added cpm_write

--- include/cpm.h
+++ include/cpm.h
@@ -17,12 +17,20 @@ to de. The value returned in register A is the return
 value. */
 extern uint8_t bdos(uint16_t bc, uint16_t de);
 
+/* Quit the current program, return to command prompt. */
 extern void cpm_term(void);
+
+/* Wait for a character from the keyboard; then echo it to the screen and return it. */
+extern uint8_t cpm_read(void);
+
+/*
+Send the character in E to the screen. Tabs are expanded to spaces. Output can be paused with ^S and restarted with ^Q (or any key
+under versions prior to CP/M 3). While the output is paused, the program can be terminated with ^C.
+*/
+extern void cpm_write(uint8_t c);
 
 /* Return a character without echoing if one is waiting; zero if none is
  * available. */
 extern uint8_t cpm_rawio(void);
-
-extern uint8_t cpm_read(void);
 
 #endif
