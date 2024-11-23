ee0dc031fe5059e0dccc179cd638981ba2a0a1d4
Dean Netherton <dinoboards@netherton.org>
2024-11-24 10:28:23 +1100

    bump docker build to version to 0.0.13

--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
-export EZ80_CLANG_TOOLCHAIN_VERSION="0.0.12"
+export EZ80_CLANG_TOOLCHAIN_VERSION="0.0.13"
 export EZ80_CLANG_TOOLCHAIN_PLATFORM="dinoboards/llvmez80-platform:${EZ80_CLANG_TOOLCHAIN_VERSION}"
 export EZ80_CLANG_TOOLCHAIN_BUILDER="dinoboards/llvmez80-builder:${EZ80_CLANG_TOOLCHAIN_VERSION}"
 
-export EZ80_CLANG_TOOLCHAIN_CACHE_FROM="--cache-from=dinoboards/llvmez80-builder:0.0.4 --cache-from=dinoboards/llvmez80:0.0.4 --cache-from=dinoboards/llvmez80-platform:0.0.4"
+export EZ80_CLANG_TOOLCHAIN_CACHE_FROM=""
 
