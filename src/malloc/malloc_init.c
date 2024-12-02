e501f8a020265f9db0322a6f3121204e5ea26b44
Dean Netherton <dinoboards@netherton.org>
2024-12-02 18:10:00 +1100

    libmalloc: init change to expect total heap size

--- src/malloc/malloc_init.c
+++ src/malloc/malloc_init.c
@@ -2,8 +2,4 @@
 #include <ez80.h>
 #include <stddef.h>
 
-void malloc_init(size_t stack_reserved) {
-  void  *sps  = _get_sps();
-  size_t size = ((unsigned int)sps - stack_reserved) - (unsigned int)(void *)_heap;
-  umm_init_heap(_heap, size);
-}
+void malloc_init(size_t heap_size) { umm_init_heap(_heap, heap_size); }
