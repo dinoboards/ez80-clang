4bff853b67c435fd38437c9f96ea97ac718b3a4e
Dean Netherton <dinoboards@netherton.org>
2024-12-12 11:46:48 +1100

    docs: more function doxygen descriptions in stdio.h

--- include/stdio.h
+++ include/stdio.h
@@ -121,12 +121,60 @@ int feof(FILE *stream);
  */
 void clearerr(FILE *stream);
 
+/**
+ * @brief Writes a string to a file stream.
+ *
+ * The fputs function writes the string `str` to the file stream `stream`.
+ *
+ * @param str The null-terminated string to be written.
+ * @param stream The file stream to write to.
+ *
+ * @return A non-negative value on success, EOF on error.
+ */
 int fputs(const char *__restrict str, FILE *__restrict stream);
 
+/**
+ * @brief Reads data from a file stream.
+ *
+ * The fread function reads `count` elements of data, each `size` bytes long,
+ * from the file stream `stream` into the array pointed to by `ptr`.
+ *
+ * @param ptr Pointer to the array where the read data will be stored.
+ * @param size Size in bytes of each element to be read.
+ * @param count Number of elements to be read.
+ * @param stream The file stream to read from.
+ *
+ * @return The number of elements successfully read, which may be less than
+ * `count` if an error or end-of-file occurs.
+ */
 size_t fread(void *ptr, size_t size, size_t count, FILE *__restrict stream);
 
+/**
+ * @brief Writes data to a file stream.
+ *
+ * The fwrite function writes `count` elements of data, each `size` bytes long,
+ * from the array pointed to by `ptr` to the file stream `stream`.
+ *
+ * @param ptr Pointer to the array containing the data to be written.
+ * @param size Size in bytes of each element to be written.
+ * @param count Number of elements to be written.
+ * @param stream The file stream to write to.
+ *
+ * @return The number of elements successfully written, which may be less than
+ * `count` if an error occurs.
+ */
 size_t fwrite(const void *__restrict ptr, size_t size, size_t count, FILE *__restrict stream);
 
+/**
+ * @brief Returns the current file position.
+ *
+ * The ftell function obtains the current value of the file position indicator
+ * for the file stream `stream`.
+ *
+ * @param stream The file stream to be queried.
+ *
+ * @return The current file position indicator on success, -1L on error.
+ */
 long ftell(FILE *stream) __attribute__((__warn_unused_result__));
 
 /**
@@ -144,23 +192,124 @@ long ftell(FILE *stream) __attribute__((__warn_unused_result__));
  */
 int fseek(FILE *stream, long offset, int origin);
 
+/**
+ * @brief Gets a character from a file stream.
+ *
+ * The fgetc function reads the next character from the file stream `stream`
+ * and returns it as an unsigned char cast to an int, or EOF on end-of-file or error.
+ *
+ * @param stream The file stream to read from.
+ *
+ * @return The character read as an unsigned char cast to an int, or EOF on end-of-file or error.
+ */
 int fgetc(FILE *stream);
+
+/**
+ * @brief Gets a character from a file stream.
+ *
+ * The getc function reads the next character from the file stream `stream`
+ * and returns it as an unsigned char cast to an int, or EOF on end-of-file or error.
+ * This macro is equivalent to the fgetc function.
+ *
+ * @param stream The file stream to read from.
+ *
+ * @return The character read as an unsigned char cast to an int, or EOF on end-of-file or error.
+ */
 #define getc(...) fgetc(__VA_ARGS__)
 
+/**
+ * @brief Writes a character to a file stream.
+ *
+ * The fputc function writes the character `c` (cast to an unsigned char) to the file stream `stream`.
+ *
+ * @param c The character to be written.
+ * @param stream The file stream to write to.
+ *
+ * @return The character written as an unsigned char cast to an int, or EOF on error.
+ */
 int fputc(int c, FILE *stream);
+
+/**
+ * @brief Writes a character to a file stream.
+ *
+ * The putc function writes the character `c` (cast to an unsigned char) to the file stream `stream`.
+ * This macro is equivalent to the fputc function.
+ *
+ * @param c The character to be written.
+ * @param stream The file stream to write to.
+ *
+ * @return The character written as an unsigned char cast to an int, or EOF on error.
+ */
 #define putc(...) fputc(__VA_ARGS__)
 
+/**
+ * @brief Gets a string from a file stream.
+ *
+ * The fgets function reads up to `num - 1` characters from the file stream `stream` and stores them into the string `str`.
+ * The string is null-terminated.
+ *
+ * @param str The array where the read string will be stored.
+ * @param num The maximum number of characters to be read (including the null terminator).
+ * @param stream The file stream to read from.
+ *
+ * @return `str` on success, NULL on error or when end-of-file occurs while no characters have been read.
+ */
 char *fgets(char *__restrict str, int num, FILE *__restrict stream);
 
+/**
+ * @brief Removes a file.
+ *
+ * The remove function deletes the file specified by `filename`.
+ *
+ * @param filename The name of the file to be deleted.
+ *
+ * @return 0 on success, non-zero on error.
+ */
 int remove(const char *filename);
 
+/**
+ * @brief Sets the file position to the beginning of the file.
+ *
+ * The rewind function sets the file position indicator for the file stream `stream` to the beginning of the file.
+ * It also clears the error and end-of-file indicators for the stream.
+ *
+ * @param stream The file stream to be rewound.
+ */
 void rewind(FILE *stream);
 
-/* standard impls */
+/**
+ * @brief Gets a character from the standard input (stdin).
+ *
+ * The getchar function reads the next character from the standard input stream (stdin)
+ * and returns it as an unsigned char cast to an int, or EOF on end-of-file or error.
+ *
+ * @return The character read as an unsigned char cast to an int, or EOF on end-of-file or error.
+ */
 int getchar(void);
 
+/**
+ * @brief Writes a character to the standard output (stdout).
+ *
+ * The putchar function writes the character specified by the argument character
+ * to the standard output stream (stdout) and returns the character written as an unsigned char cast to an int,
+ * or EOF on error.
+ *
+ * @param character The character to be written.
+ *
+ * @return The character written as an unsigned char cast to an int, or EOF on error.
+ */
 int putchar(int character);
 
+/**
+ * @brief Writes a string to the standard output (stdout).
+ *
+ * The puts function writes the string pointed to by str to the standard output stream (stdout)
+ * and appends a newline character. It returns a non-negative number on success, or EOF on error.
+ *
+ * @param str The string to be written.
+ *
+ * @return A non-negative number on success, or EOF on error.
+ */
 int puts(const char *str);
 
 /**
@@ -181,24 +330,131 @@ int puts(const char *str);
  */
 int printf(const char *__restrict format, ...) __attribute__((format(__printf__, 1, 2)));
 
+/**
+ * @brief Prints formatted output to the standard output (stdout) using a variable argument list.
+ *
+ * The vprintf function formats and prints a series of characters and values to the standard output stream (stdout)
+ * using the format string and the variable argument list provided.
+ *
+ * @param format The format string.
+ * @param va The variable argument list.
+ *
+ * @return The number of characters printed, or a negative value on error.
+ */
 int vprintf(const char *__restrict format, va_list va) __attribute__((format(__printf__, 1, 0)));
 
+/**
+ * @brief Prints formatted output to a string using a variable argument list.
+ *
+ * The vsprintf function formats and stores a series of characters and values in the buffer provided
+ * using the format string and the variable argument list.
+ *
+ * @param buffer The buffer to store the formatted string.
+ * @param format The format string.
+ * @param va The variable argument list.
+ *
+ * @return The number of characters written, excluding the null terminator, or a negative value on error.
+ */
 int vsprintf(char *__restrict buffer, const char *__restrict format, va_list va) __attribute__((format(__printf__, 1, 0)));
 
+/**
+ * @brief Prints formatted output to a string with a limit on the number of characters.
+ *
+ * The snprintf function formats and stores a series of characters and values in the buffer provided,
+ * with a limit on the number of characters, using the format string and additional arguments.
+ *
+ * @param buffer The buffer to store the formatted string.
+ * @param count The maximum number of characters to write, including the null terminator.
+ * @param format The format string.
+ * @param ... Additional arguments specifying the data to print.
+ *
+ * @return The number of characters that would have been written if count was sufficiently large, not counting the null terminator.
+ */
 int snprintf(char *buffer, size_t count, const char *__restrict format, ...) __attribute__((format(__printf__, 3, 4)));
 
+/**
+ * @brief Prints formatted output to a string with a limit on the number of characters using a variable argument list.
+ *
+ * The vsnprintf function formats and stores a series of characters and values in the buffer provided,
+ * with a limit on the number of characters, using the format string and the variable argument list.
+ *
+ * @param buffer The buffer to store the formatted string.
+ * @param count The maximum number of characters to write, including the null terminator.
+ * @param format The format string.
+ * @param va The variable argument list.
+ *
+ * @return The number of characters that would have been written if count was sufficiently large, not counting the null terminator.
+ */
 int vsnprintf(char *buffer, size_t count, const char *__restrict format, va_list va) __attribute__((format(__printf__, 3, 0)));
 
+/**
+ * @brief Prints formatted output to a string.
+ *
+ * The sprintf function formats and stores a series of characters and values in the buffer provided
+ * using the format string and additional arguments.
+ *
+ * @param buffer The buffer to store the formatted string.
+ * @param format The format string.
+ * @param ... Additional arguments specifying the data to print.
+ *
+ * @return The number of characters written, excluding the null terminator.
+ */
 int sprintf(char *__restrict buffer, const char *__restrict format, ...) __attribute__((format(__printf__, 2, 3)));
 
+/**
+ * @brief Prints formatted output to a file stream.
+ *
+ * The fprintf function formats and prints a series of characters and values to the specified file stream
+ * using the format string and additional arguments.
+ *
+ * @param stream The file stream to write to.
+ * @param format The format string.
+ * @param ... Additional arguments specifying the data to print.
+ *
+ * @return The number of characters printed, or a negative value on error.
+ */
 int fprintf(FILE *stream, const char *__restrict format, ...) __attribute__((format(__printf__, 2, 3)));
 
+/**
+ * @brief Prints formatted output to a file stream using a variable argument list.
+ *
+ * The vfprintf function formats and prints a series of characters and values to the specified file stream
+ * using the format string and the variable argument list provided.
+ *
+ * @param stream The file stream to write to.
+ * @param format The format string.
+ * @param va The variable argument list.
+ *
+ * @return The number of characters printed, or a negative value on error.
+ */
 int vfprintf(FILE *stream, const char *__restrict format, va_list va) __attribute__((format(__printf__, 2, 0)));
 
 typedef size_t rsize_t;
 
+/**
+ * @brief Reads a line from the standard input (stdin) into a buffer with bounds checking.
+ *
+ * The gets_s function reads a line from the standard input stream (stdin) into the buffer pointed to by str,
+ * up to a maximum of n-1 characters, and null-terminates the string. It ensures that the buffer is not overflowed.
+ *
+ * @param str The buffer to store the input string.
+ * @param n The maximum number of characters to read, including the null terminator.
+ *
+ * @return A pointer to the buffer containing the input string, or NULL on error.
+ */
 char *gets_s(char *__restrict str, rsize_t n);
-int   scanf(const char *format, ...);
+
+/**
+ * @brief Reads formatted input from the standard input (stdin).
+ *
+ * The scanf function reads data from the standard input stream (stdin) and stores them according to the format string.
+ *
+ * @param format The format string.
+ * @param ... Additional arguments specifying the data to read.
+ *
+ * @return The number of input items successfully matched and assigned, or EOF on error or end-of-file.
+ */
+int scanf(const char *format, ...);
 
 /**
  * @brief Reads formatted input from a string.
@@ -219,12 +475,65 @@ int   scanf(const char *format, ...);
  */
 int sscanf(const char *s, const char *format, ...);
 
-int   fscanf(FILE *fp, const char *fmt, ...);
-FILE *freopen(const char *__restrict filename, const char *__restrict mode, FILE *stream);
-int   ungetc(int c, FILE *stream);
+/**
+ * @brief Reads formatted input from a file stream.
+ *
+ * The fscanf function reads data from the specified file stream and stores them according to the format string.
+ *
+ * @param fp The file stream to read from.
+ * @param fmt The format string.
+ * @param ... Additional arguments specifying the data to read.
+ *
+ * @return The number of input items successfully matched and assigned, or EOF on error or end-of-file.
+ */
+int fscanf(FILE *fp, const char *fmt, ...);
 
+/**
+ * @brief Reopens a file stream with a different file or mode.
+ *
+ * The freopen function opens the file specified by filename and associates it with the specified file stream,
+ * which is then reopened with the specified mode.
+ *
+ * @param filename The name of the file to open.
+ * @param mode The mode to open the file with.
+ * @param stream The file stream to associate with the file.
+ *
+ * @return A pointer to the reopened file stream, or NULL on error.
+ */
+FILE *freopen(const char *__restrict filename, const char *__restrict mode, FILE *stream);
+
+/**
+ * @brief Pushes a character back onto a file stream.
+ *
+ * The ungetc function pushes the character specified by c back onto the specified file stream,
+ * making it available for subsequent reads.
+ *
+ * @param c The character to push back.
+ * @param stream The file stream to push the character onto.
+ *
+ * @return The character pushed back, or EOF on error.
+ */
+int ungetc(int c, FILE *stream);
+
+/**
+ * @brief Prints a descriptive error message to the standard error stream (stderr).
+ *
+ * The perror function prints a descriptive error message to the standard error stream (stderr),
+ * based on the string provided and the current value of the global variable errno.
+ *
+ * @param s The custom message to print before the error description.
+ */
 void perror(const char *s);
 
+/**
+ * @brief Gets the file descriptor associated with a file stream.
+ *
+ * The fileno function returns the file descriptor associated with the specified file stream.
+ *
+ * @param stream The file stream to get the file descriptor for.
+ *
+ * @return The file descriptor associated with the file stream.
+ */
 static inline int fileno(const FILE *stream) { return (int)stream; }
 
 __END_DECLS
--- include/stdlib.h
+++ include/stdlib.h
@@ -68,60 +68,325 @@ void *realloc(void *ptr, size_t size) __attribute__((warn_unused_result));
  * @param ptr Pointer to the memory to be freed.
  */
 void free(void *ptr) __NOEXCEPT;
-
+/**
+ * @brief Converts a string to a double.
+ *
+ * The atof function converts the initial portion of the string pointed to by nptr to a double.
+ *
+ * @param nptr The string to be converted.
+ *
+ * @return The converted value as a double.
+ */
 double atof(const char *nptr) __attribute__((nonnull(1)));
 
+/**
+ * @brief Converts a string to an integer.
+ *
+ * The atoi function converts the initial portion of the string pointed to by nptr to an int.
+ *
+ * @param nptr The string to be converted.
+ *
+ * @return The converted value as an int.
+ */
 int atoi(const char *nptr) __attribute__((nonnull(1)));
 
+/**
+ * @brief Converts a string to a long integer.
+ *
+ * The atol function converts the initial portion of the string pointed to by nptr to a long.
+ *
+ * @param nptr The string to be converted.
+ *
+ * @return The converted value as a long.
+ */
 long atol(const char *nptr) __attribute__((nonnull(1)));
 
+/**
+ * @brief Converts a string to a long long integer.
+ *
+ * The atoll function converts the initial portion of the string pointed to by nptr to a long long.
+ *
+ * @param nptr The string to be converted.
+ *
+ * @return The converted value as a long long.
+ */
 long long atoll(const char *nptr) __attribute__((nonnull(1)));
 
+/**
+ * @brief Converts a string to a float.
+ *
+ * The strtof function converts the initial portion of the string pointed to by nptr to a float.
+ * It also sets the value of endptr to point to the character after the last character used in the conversion.
+ *
+ * @param nptr The string to be converted.
+ * @param endptr A pointer to a pointer to character.
+ *
+ * @return The converted value as a float.
+ */
 float strtof(const char *__restrict nptr, char **__restrict endptr) __attribute__((nonnull(1)));
 
+/**
+ * @brief Converts a string to a double.
+ *
+ * The strtod function converts the initial portion of the string pointed to by nptr to a double.
+ * It also sets the value of endptr to point to the character after the last character used in the conversion.
+ *
+ * @param nptr The string to be converted.
+ * @param endptr A pointer to a pointer to character.
+ *
+ * @return The converted value as a double.
+ */
 double strtod(const char *__restrict nptr, char **__restrict endptr) __attribute__((nonnull(1)));
 
+/**
+ * @brief Converts a string to a long integer.
+ *
+ * The strtol function converts the initial portion of the string pointed to by nptr to a long.
+ * It also sets the value of endptr to point to the character after the last character used in the conversion.
+ * The conversion is done according to the given base, which must be between 2 and 36 inclusive, or be the special value 0.
+ *
+ * @param nptr The string to be converted.
+ * @param endptr A pointer to a pointer to character.
+ * @param base The base to use for the conversion.
+ *
+ * @return The converted value as a long.
+ */
 long strtol(const char *__restrict nptr, char **__restrict endptr, int base) __attribute__((nonnull(1)));
 
+/**
+ * @brief Converts a string to a long long integer.
+ *
+ * The strtoll function converts the initial portion of the string pointed to by nptr to a long long.
+ * It also sets the value of endptr to point to the character after the last character used in the conversion.
+ * The conversion is done according to the given base, which must be between 2 and 36 inclusive, or be the special value 0.
+ *
+ * @param nptr The string to be converted.
+ * @param endptr A pointer to a pointer to character.
+ * @param base The base to use for the conversion.
+ *
+ * @return The converted value as a long long.
+ */
 long long strtoll(const char *__restrict nptr, char **__restrict endptr, int base) __attribute__((nonnull(1)));
 
+/**
+ * @brief Converts a string to an unsigned long integer.
+ *
+ * The strtoul function converts the initial portion of the string pointed to by nptr to an unsigned long.
+ * It also sets the value of endptr to point to the character after the last character used in the conversion.
+ * The conversion is done according to the given base, which must be between 2 and 36 inclusive, or be the special value 0.
+ *
+ * @param nptr The string to be converted.
+ * @param endptr A pointer to a pointer to character.
+ * @param base The base to use for the conversion.
+ *
+ * @return The converted value as an unsigned long.
+ */
 unsigned long strtoul(const char *__restrict nptr, char **__restrict endptr, int base) __attribute__((nonnull(1)));
 
+/**
+ * @brief Converts a string to an unsigned long long integer.
+ *
+ * The strtoull function converts the initial portion of the string pointed to by nptr to an unsigned long long.
+ * It also sets the value of endptr to point to the character after the last character used in the conversion.
+ * The conversion is done according to the given base, which must be between 2 and 36 inclusive, or be the special value 0.
+ *
+ * @param nptr The string to be converted.
+ * @param endptr A pointer to a pointer to character.
+ * @param base The base to use for the conversion.
+ *
+ * @return The converted value as an unsigned long long.
+ */
 unsigned long long strtoull(const char *__restrict nptr, char **__restrict endptr, int base) __attribute__((nonnull(1)));
 
+/**
+ * @brief Sets the seed for the random number generator.
+ *
+ * The srand function sets the seed for the random number generator used by the rand function.
+ *
+ * @param seed The seed value.
+ */
 void srand(unsigned int seed);
 
+/**
+ * @brief Generates a random number.
+ *
+ * The rand function returns a pseudo-random number in the range of 0 to RAND_MAX.
+ *
+ * @return A pseudo-random number in the range of 0 to RAND_MAX.
+ */
 int rand(void);
 
+/**
+ * @brief Performs a binary search on a sorted array.
+ *
+ * The bsearch function searches for a key in a sorted array pointed to by base.
+ * The array contains nmemb elements, each of size bytes. The comparison function compar is used to compare elements.
+ *
+ * @param key The key to search for.
+ * @param base The base of the array to search.
+ * @param nmemb The number of elements in the array.
+ * @param size The size of each element in the array.
+ * @param compar The comparison function.
+ *
+ * @return A pointer to the matching element, or NULL if no match is found.
+ */
 void *bsearch(void *key, void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *))
     __attribute__((nonnull(1, 2, 5)));
-
+/**
+ * @brief Sorts an array.
+ *
+ * The qsort function sorts an array pointed to by base. The array contains nmemb elements, each of size bytes.
+ * The comparison function compar is used to compare elements.
+ *
+ * @param base The base of the array to sort.
+ * @param nmemb The number of elements in the array.
+ * @param size The size of each element in the array.
+ * @param compar The comparison function.
+ */
 void qsort(void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *)) __attribute__((nonnull(1, 4)));
 
+/**
+ * @brief Causes abnormal program termination.
+ *
+ * The abort function causes abnormal program termination without cleaning up.
+ */
 void abort(void) __NOEXCEPT __attribute__((noreturn));
 
+/**
+ * @brief Registers a function to be called at normal program termination.
+ *
+ * The atexit function registers the function pointed to by func to be called at normal program termination.
+ *
+ * @param func The function to be called at normal program termination.
+ *
+ * @return Zero on success, or a non-zero value on error.
+ */
 int atexit(void (*)(void)) __NOEXCEPT;
 
+
+/**
+ * @brief Registers a function to be called at normal program termination.
+ *
+ * The on_exit function registers the function pointed to by func to be called at normal program termination.
+ * The function is passed the exit status and the argument arg.
+ *
+ * @param func The function to be called at normal program termination.
+ * @param arg The argument to be passed to the function.
+ *
+ * @return Zero on success, or a non-zero value on error.
+ */
 int on_exit(void (*)(int, void *), void *) __NOEXCEPT;
 
+
+/**
+ * @brief Causes normal program termination.
+ *
+ * The exit function causes normal program termination and performs cleanup.
+ *
+ * @param status The exit status.
+ */
 void exit(int) __NOEXCEPT __attribute__((noreturn));
 
+/**
+ * @brief Registers a function to be called at quick program termination.
+ *
+ * The at_quick_exit function registers the function pointed to by func to be called at quick program termination.
+ *
+ * @param func The function to be called at quick program termination.
+ *
+ * @return Zero on success, or a non-zero value on error.
+ */
 int at_quick_exit(void (*)(void)) __NOEXCEPT;
 
+
+/**
+ * @brief Causes quick program termination.
+ *
+ * The quick_exit function causes quick program termination without performing cleanup.
+ *
+ * @param status The exit status.
+ */
 void quick_exit(int) __NOEXCEPT __attribute__((noreturn));
 
+/**
+ * @brief Causes immediate program termination.
+ *
+ * The _Exit function causes immediate program termination without performing cleanup.
+ *
+ * @param status The exit status.
+ */
 void _Exit(int) __NOEXCEPT __attribute__((noreturn));
 
+/**
+ * @brief Computes the absolute value of an integer.
+ *
+ * The abs function computes the absolute value of the integer n.
+ *
+ * @param n The integer whose absolute value is to be computed.
+ *
+ * @return The absolute value of the integer n.
+ */
 int abs(int n);
 
+/**
+ * @brief Computes the absolute value of a long integer.
+ *
+ * The labs function computes the absolute value of the long integer n.
+ *
+ * @param n The long integer whose absolute value is to be computed.
+ *
+ * @return The absolute value of the long integer n.
+ */
 long labs(long n);
 
+/**
+ * @brief Computes the absolute value of a long long integer.
+ *
+ * The llabs function computes the absolute value of the long long integer n.
+ *
+ * @param n The long long integer whose absolute value is to be computed.
+ *
+ * @return The absolute value of the long long integer n.
+ */
 long long llabs(long long n);
 
+/**
+ * @brief Computes the quotient and remainder of integer division.
+ *
+ * The div function computes the quotient and remainder of the division of numer by denom.
+ * The result is returned as a structure of type div_t, which contains two members: quot (the quotient) and rem (the remainder).
+ *
+ * @param numer The numerator.
+ * @param denom The denominator.
+ *
+ * @return A structure of type div_t containing the quotient and remainder.
+ */
 div_t div(int numer, int denom);
 
+/**
+ * @brief Computes the quotient and remainder of long integer division.
+ *
+ * The ldiv function computes the quotient and remainder of the division of numer by denom.
+ * The result is returned as a structure of type ldiv_t, which contains two members: quot (the quotient) and rem (the remainder).
+ *
+ * @param numer The numerator.
+ * @param denom The denominator.
+ *
+ * @return A structure of type ldiv_t containing the quotient and remainder.
+ */
 ldiv_t ldiv(long numer, long denom);
 
+/**
+ * @brief Computes the quotient and remainder of long long integer division.
+ *
+ * The lldiv function computes the quotient and remainder of the division of numer by denom.
+ * The result is returned as a structure of type lldiv_t, which contains two members: quot (the quotient) and rem (the remainder).
+ *
+ * @param numer The numerator.
+ * @param denom The denominator.
+ *
+ * @return A structure of type lldiv_t containing the quotient and remainder.
+ */
 lldiv_t lldiv(long long numer, long long denom);
 
 __END_DECLS
